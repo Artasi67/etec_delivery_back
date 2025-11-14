94b7ca5a5c519bd58be9c28f3fcf44051b0c6fde
Artasi67 <athurtavaressilva@gmail.com>
2025-11-14 08:46:19 -0300

    tudo certo acho

---
+++ Data/DbConfig.cs
@@ -0,0 +1,26 @@
+﻿using etec_delivery_backend.Models;
+using Microsoft.EntityFrameworkCore;
+namespace etec_delivery_back.Data
+{
+    public class DbConfig : DbContext
+    {
+        public DbConfig(DbContextOptions<DbConfig> options) : base(options) 
+        {
+            public DbSet<Usuario> Usuarios { get; set; }
+            public DbSet<RotaEntrega> Entrega { get; set; }
+            public DbSet<Cliente> Cliente { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>(
+                entity =>
+                {
+                    entity.HasIndex(e => e.Email_Usuario).IsUnique();
+                }
+                );
+        }
+    }
+    }
+}
--- Services/ServiceGoogleMaps.cs
+++ Services/ServiceGoogleMaps.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Text.Json;
 
 namespace etec_delivery_back.Services
 {
@@ -7,11 +8,48 @@ namespace etec_delivery_back.Services
     {
         Task<(Double? Lat, Double? Lng)> PegarCoordenadasDoEnderecoAsync(string endereco);
     }
+
     public class ServiceGoogleMaps : IServiceGoogleMaps
-    {
-        public Task<(double? Lat, double? Lng)> PegarCoordenadasDoEnderecoAsync(string endereco)
+    {        
+        private readonly HttpClient _httpClient;
+        private readonly string _api_key;
+
+    public ServiceGoogleMaps(HttpClient httpClient, IConfiguration config)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+            _api_key = config["GoogleMaps:ApiKey"];
+        }
+
+    public async Task<(double? Lat, double? Lng)> PegarCoordenadasDoEnderecoAsync(string endereco)
+        {
+            if (string.IsNullOrEmpty(_api_key))
+            {
+                throw new InvalidOperationException("A chave de API do Google Maps não foi configurada.");
+            }
+            var uri_requisicao = $"https://maps.google.com/maps/api/geocode/json?adress={Uri.EscapeDataString(_api_key)}";
+
+            try
+            {
+                var resposta = await _httpClient.GetAsync(uri_requisicao);
+                resposta.EnsureSuccessStatusCode();
+
+                var conteudo = await resposta.Content.ReadAsStringAsync();
+                var resposta_geocode = JsonSerializer.Deserialize<GeocodeResponse>(conteudo,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+
+                if(resposta_geocode?.Status == "OK" && resposta_geocode.Results.Any())
+                {
+                    var localizacao = resposta_geocode.Results.First().Geometry.Location;
+                    return (localizacao.Lat, localizacao.Lng);
+                }
+
+                return (null, null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao comunicar com a API." + ex.Message);
+                return (null, null);
+            }
         }
     }
 
